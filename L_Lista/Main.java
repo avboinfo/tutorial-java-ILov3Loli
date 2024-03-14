47b3320958d07735945a67ff279192c0c345a87a
ILov3Loli <150423785+ILov3Loli@users.noreply.github.com>
2024-03-14 12:26:39 +0000

    addHead-addSorted

--- L_Lista/Main.java
+++ L_Lista/Main.java
@@ -8,9 +8,21 @@ public class Main {
         /*Nodo n = new Nodo(100,null);   elementi del nodo
         System.out.println(n);*/
  
-        for(int i=0;i<10;i++){
-            l.add(new Nodo(i,null));
+        /* for(int i=0;i<10;i++){
+            l.addTail(new Nodo(i,null));
         }
+        l.addHead(new Nodo(2024,null));  */
+
+        l.addSorted(new Nodo(300,null));
+        l.addSorted(new Nodo(250,null));
+        l.addSorted(new Nodo(350,null));
+        l.addSorted(new Nodo(320,null));
+        l.addSorted(new Nodo(100,null));
+        l.addSorted(new Nodo(170,null));
+        l.addSorted(new Nodo(290,null));
+        l.addSorted(new Nodo(450,null));
+
+
         System.out.println(l);
     }
 }
